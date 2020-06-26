d5809ec02197f29ec9abef4eeaba8d60a79b2615
JT Park <bajutae@gmail.com>
2020-06-27 01:51:55 +0900

    add k8s yaml

--- devops.sh
+++ devops.sh
@@ -20,6 +20,26 @@ deploy() {
   docker ps 
 }
 
+k8s_deploy() {
+  echo
+  echo "## Strartig k8s yaml . ##"
+  echo "## install ingress nginx controller## "
+  kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud/deploy.yaml
+  kubectl apply -f ./k8s
+  echo
+  echo "## k8s apply Complieted! ##"
+}
+
+k8s_delete() {
+  echo
+  echo "## Delete k8s yaml . ##"
+  echo
+  kubectl delete svc,deploy petclinic
+  kubectl delete svc,deploy,configmap mysql
+  kubectl delete namespace ingress-nginx
+  echo
+  echo "## k8s Delete Complieted! ##"
+}
 case "$1" in
   start)
     start
@@ -36,7 +56,13 @@ case "$1" in
   deploy)
     deploy
     ;;
+  k8s_deploy)
+    k8s_deploy
+    ;;
+  k8s_delete)
+    k8s_delete
+    ;;
 *)
-  echo "Usage: $0 {start | stop | restart | status | deploy}"
+  echo "Usage: $0 {start | stop | restart | status | deploy | k8s_deploy | k8s_delete}"
 esac
 exit 0
